e0177abdd00f9a2d4b278e656db429ae0a7845c5
Jennifer <k43wu@uwaterloo.ca>
2018-02-28 21:26:36 -0500

    Added the basic version of MainGameActivity layout view.

--- NumericonsAndroid/app/src/main/java/sen/sen/numericonsandroid/Activities/MainActivity.java
+++ NumericonsAndroid/app/src/main/java/sen/sen/numericonsandroid/Activities/MainActivity.java
@@ -1,8 +1,11 @@
 package sen.sen.numericonsandroid.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import sen.sen.numericonsandroid.Networking.WebsocketController;
 import sen.sen.numericonsandroid.R;
@@ -12,11 +15,12 @@ public class MainActivity extends AppCompatActivity{
   @Override
   protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_main);
   }
 
   public void onButtonPressed(View view){
-
-
+    Intent intent = new Intent(this, MainGameActivity.class);
+    startActivity(intent);
   }
 }
---
+++ NumericonsAndroid/app/src/main/java/sen/sen/numericonsandroid/Activities/MainGameActivity.java
@@ -0,0 +1,21 @@
+package sen.sen.numericonsandroid.Activities;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import sen.sen.numericonsandroid.R;
+
+public class MainGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main_game);
+    }
+}
