5a246c1efe492e766b9c5079fb3d9c73550071c7
sen-yang <sen.yang2595@gmail.com>
2018-03-18 23:50:36 -0400

    Added bluetooth chat service.

--- NumericonsAndroid/app/src/main/java/sen/sen/numericonsandroid/Global/Constants.java
+++ NumericonsAndroid/app/src/main/java/sen/sen/numericonsandroid/Global/Constants.java
@@ -110,4 +110,16 @@ public class Constants{
     RUNNING,
     FINISHED
   }
+
+
+  //bluetooth messaging constants
+
+  public static final int MESSAGE_STATE_CHANGE = 1;
+  public static final int MESSAGE_READ = 2;
+  public static final int MESSAGE_WRITE = 3;
+  public static final int MESSAGE_DEVICE_NAME = 4;
+  public static final int MESSAGE_TOAST = 5;
+
+  public static final String DEVICE_NAME = "device_name";
+  public static final String TOAST = "toast";
 }
